212e6e8f6150a04216f353e5eb5285bcc8077e8b
Tomasz <tomaszburas.dev@gmail.com>
2024-03-27 21:59:21 +0100

    feat(api): added getTags()

---
+++ src/api/getTags.ts
@@ -0,0 +1,23 @@
+import { StackExchangeUrl } from "@/config";
+import { type Response, type StackExchangeError, type Tag } from "@/types";
+
+export const getTags = async (): Promise<Response<Tag[]>> => {
+	try {
+		const response = await fetch(
+			`${StackExchangeUrl}/tags?order=desc&sort=popular&site=stackoverflow`,
+		);
+		const data = (await response.json()) as { items: Tag[] } | StackExchangeError;
+
+		if ("error_id" in data) {
+			throw new Error(data.error_message);
+		}
+
+		return { ok: true, data: data.items };
+	} catch (error) {
+		console.error(error);
+		if (error instanceof Error) {
+			return { ok: false, message: error.message };
+		}
+		return { ok: false, message: "An unknown error occurred" };
+	}
+};
