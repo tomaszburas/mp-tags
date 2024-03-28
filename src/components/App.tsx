76f6289943c42c01600ba8a34f0a30ad7876f6ce
Tomasz <tomaszburas.dev@gmail.com>
2024-03-28 10:58:32 +0100

    refactor: app component to include TagsTable and TagsProvider

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,3 +1,14 @@
+import { TagsTable } from "@/components/TagsTable";
+import { TagsProvider } from "@/context/TagsContext";
+
 export const App = () => {
-	return <div>Hello world!</div>;
+	return (
+		<section className="flex w-full flex-col items-center">
+			<TagsProvider>
+				<div className="w-96">
+					<TagsTable />
+				</div>
+			</TagsProvider>
+		</section>
+	);
 };
