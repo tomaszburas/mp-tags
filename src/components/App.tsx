be96b1b188352b4448b22a29126b1e9f89fd2604
Tomasz <tomaszburas.dev@gmail.com>
2024-03-28 18:22:32 +0100

    refactor: app component layout

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -5,14 +5,14 @@ import { TagsProvider } from "@/context/TagsContext";
 
 export const App = () => {
 	return (
-		<section className="sm:py lx:max-w-7xl mx-auto flex h-screen max-w-md flex-col items-center px-4 sm:max-w-2xl md:max-w-4xl lg:max-w-7xl">
+		<main className="mx-auto flex h-screen max-w-md flex-col items-center p-4 sm:max-w-2xl md:max-w-4xl lg:max-w-7xl">
 			<Navigation />
 			<TagsProvider>
-				<div className="mb-4 w-96">
+				<section className="my-4 w-96">
 					<TagsTable />
-				</div>
+				</section>
 			</TagsProvider>
 			<Footer />
-		</section>
+		</main>
 	);
 };
